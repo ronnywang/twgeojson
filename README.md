5d55eb307ee76b316a5b8acd317ff0b586c9be8e
Ronny Wang <ronnywang@gmail.com>
2012-11-29 23:02:51 +0800

    加上台灣村里疆界資料

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ twgeojson
 =========
 
 行政區域疆界
+
+資料來源: [國土資訊圖資資訊中心 - 全國村里界圖（台澎金馬）-經緯度](http://tgos.nat.gov.tw/tgos/Web/Metadata/TGOS_MetaData_View.aspx?MID=36646&SHOW_BACK_BUTTON=false)
+
+資料時間: 2012/11/13
+
+這邊只有放里界資訊
+如果要縣界和鄉界的話，可以到 [https://github.com/g0v/twgeojson](https://github.com/g0v/twgeojson)
+
+檔案說明:
+- output.json.gz 輸出結果檔，一共 8052 村里，檔案大小 gzip 後還有 30MB 左右
+- output.sample.json 跟 output.json 格式一樣，只列出 10 個村里讓大家看看格式。
+- parse.php 將 shp 處理成 geojson 的程式，需要有 shp2pgsql (要裝 postgis)
+
+只需要將國土資訊圖資資訊中心的原表抓下來，再用 parse.php xxx.shp 就會產生 output.json 和 output.sample.json 了
---
+++ parse.php
@@ -0,0 +1,173 @@
+<?php
+
+ini_set('memory_limit', '2048m');
+
+class Parser
+{
+    public function fetchByte($size)
+    {
+        $ret = substr($this->data, 0, $size);
+        $this->data = substr($this->data, $size);
+        return $ret;
+    }
+
+    /**
+     * 把 Well-Known-Binary 轉換成 GeoJSON，程式簡化自 https://github.com/phayes/geoPHP
+     *
+     * @param string $value 16進位字串
+     * @access public
+     * @return Object
+     */
+    public function parseWKB($value)
+    {
+        $this->data = pack('H*', $value);
+
+        return $this->getGeometry();
+    }
+
+    public function getGeometry()
+    {
+        $base_info = unpack("corder/ctype/cz/cm/cs", $this->fetchByte(5));
+
+        $ret = new StdClass;
+        switch ($base_info['type']) {
+        case 1:
+            $ret->type = 'Point';
+            $ret->coordinates = $this->getPoint();
+            break;
+        case 2:
+            $ret->type = 'LineString';
+            $ret->coordinates = $this->getLinstring();
+            break;
+        case 3:
+            $ret->type = 'Polygon';
+            $ret->coordinates = $this->getPolygon();
+            break;
+        case 4:
+            $ret->type = 'MultiPoint';
+            $ret->coordinates = $this->getMulti('point');
+            break;
+        case 5:
+            $ret->type = 'MultiLineString';
+            $ret->coordinates = $this->getMulti('line');
+            break;
+        case 6:
+            $ret->type = 'MultiPolygon';
+            $ret->coordinates = $this->getMulti('polygon');
+            break;
+        case 7:
+            $ret->type = 'GeometryCollection';
+            $ret->coordinates = $this->getMulti('geometry');
+            break;
+        }
+        return $ret;
+    }
+
+    public function getPoint() {
+        $point_coords = unpack("d*", $this->fetchByte(32));
+        return [$point_coords[1], $point_coords[2]];
+    }
+
+    public function getLinstring() {
+        $line_length = unpack('L', $this->fetchByte(4));
+
+        if (!$line_length[1]) {
+            return [];
+        }
+
+        $line_coords = unpack('d*', $this->fetchByte($line_length[1] * 32));
+
+        $components = array();
+        $i = 1;
+        $num_coords = count($line_coords);
+        while ($i <= $num_coords) {
+            $components[] = [$line_coords[$i], $line_coords[$i + 1]];
+            $i += 4;
+        }
+        return $components;
+    }
+
+    public function getPolygon() {
+        $poly_length = unpack('L', $this->fetchByte(4));
+
+        $components = array();
+        $i = 1;
+        while ($i <= $poly_length[1]) {
+            $components[] = $this->getLinstring();
+            $i++;
+        }
+        return $components;
+    }
+
+    public function getMulti($type) {
+        $multi_length = unpack('L', $this->fetchByte(4));
+
+        $components = array();
+        $i = 1;
+        while ($i <= $multi_length[1]) {
+            if ('geometry' == $type) {
+                $components[] = $this->getGeometry();
+            } else {
+                $components[] = $this->getGeometry()->coordinates;
+            }
+            $i++;
+        }
+
+        return $components;
+    }
+
+    public function combine($columns, $values)
+    {
+        $numeric = ['area', 'max_x', 'max_y', 'min_x', 'min_y', 'x', 'y', 'sort', 'shape_leng', 'shape_le_1', 'shape_area'];
+        $int = ['objectid', 'oorig_fid'];
+
+        $obj = new StdClass;
+        foreach (array_combine($columns, $values) as $k => $v) {
+            if (in_array($k, $numeric)) {
+                $obj->{$k} = doubleval($v);
+            } elseif (in_array($k, $int)) {
+                $obj->{$k} = intval($v);
+            } elseif ('the_geom' == $k) {
+                $obj->{$k} = $this->parseWKB($v);
+            } else {
+                $obj->{$k} = $v;
+            }
+        }
+        return $obj;
+    }
+
+    public function main($file)
+    {
+        $fp = popen('shp2pgsql -W big5-2003 ' . escapeshellarg($file), 'r');
+        $villages = [];
+        while (false !== ($line = fgets($fp))) {
+            if (!preg_match('#^INSERT INTO ".*" \((.*)\) VALUES \((.*)\)#', $line, $matches)) {
+                continue;
+            }
+            $columns = array_map(function($r) {
+                return trim($r, '"');
+            }, explode(',', $matches[1]));
+            $values = array_map(function($r) {
+                return NULL == $r ? null : trim($r, '\'');
+            }, explode(',', $matches[2]));
+            $village = $this->combine($columns, $values);
+            $villages[] = $village;
+        }
+        $json = new StdClass;
+        $json->link = 'https://github.com/ronnywang/twgeojson';
+        $json->data_time = '2012-11-13';
+        $json->data_source = 'http://tgos.nat.gov.tw/tgos/Web/Metadata/TGOS_MetaData_View.aspx?MID=36646&SHOW_BACK_BUTTON=false';
+        $json->description = '全國村里界圖（台澎金馬）-經緯度';
+        $json->data = $villages;
+
+        file_put_contents('output.json', json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        $json->data = array_slice($json->data, 0, 10);
+        file_put_contents('output.sample.json', json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
+
+if (!$file = $_SERVER['argv'][1]) {
+    die("請用 parseshp.php [file.shp]\n");
+}
+$parser = new Parser;
+$parser->main($file);
